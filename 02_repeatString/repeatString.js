75092c34e03d28d4c75cce1dd4e3a0ebbc6374e7
CrispyTelomeres <50087826+CrispyTelomeres@users.noreply.github.com>
2023-08-02 13:35:18 -0600

    Completed 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, iterations) {   
+    if (iterations < 0) {
+        return "ERROR";
+    } else {
+        let answer = "";
+        for (let i = 0; i < iterations; i++) {
+            answer += string;
+        }
+        return answer;
+    }   
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let x = 0;
+    let first = "";
+    let last = "";
+    if (typeof a != "number" || typeof b != "number" || a < 0 || b < 0) {
+        return "ERROR";
+    } else if (a < b) {
+        first = a;
+        last = b;
+    } else {
+        first = b
+        last = a;
+    }
+    for (let i = first; i <= last; i++) {
+        x += i;
+    }
+    return x;
 
 };
 
